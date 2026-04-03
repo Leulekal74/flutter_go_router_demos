7981bbb866cf4a1fbcdef19106767f55a655ea78
leulekal <walelgnleulekal@gmail.com>
2026-04-03 20:57:46 +0300

    Add all 3 Flutter GoRouter lab assignments and screenshots

---
+++ go_routing_demo3/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+void main() => runApp(const MyApp());
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      name: 'home',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/details',
+      name: 'details',
+      builder: (context, state) => const DetailsScreen(),
+    ),
+  ],
+);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'go_routing_demo3',
+      routerConfig: _router,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: Center(
+        child: ElevatedButton(
+          // Navigating using the route name 'details'
+          onPressed: () => context.goNamed('details'),
+          child: const Text('Go to Details'),
+        ),
+      ),
+    );
+  }
+}
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Details')),
+      body: const Center(
+        child: Text('This is the details screen!'),
+      ),
+    );
+  }
+}
